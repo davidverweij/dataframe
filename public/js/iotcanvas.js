cbbbe250bba828fb88d12edab2ddd576cafbd929
David Verweij <davidverweij@gmail.com>
2020-01-22 17:20:21 +0000

    finalized touch and drag functionality. Used scaled position delta to determine precision and checked led hits based on position and formula (assuming always hit within the matrix) rather than iterating over objects

--- public/js/iotcanvas.js
+++ public/js/iotcanvas.js
@@ -1,5 +1,3 @@
-
-
 /*
 TODO: Convert RGBW to 2 character HEX values, to be stored in the database.
 
@@ -29,74 +27,92 @@ led[i].b = decodeNibble(data[offset + 4]) * 16 + decodeNibble(data[offset + 5]);
 
 */
 let initialLoad = false;
-let p5sketch;     //reference to P5js sketch
+let p5sketch; //reference to P5js sketch
 
-loadTestMode = function(){
-  p5sketch = new p5(sketch, 'main');
+loadTestMode = function() {
+  p5sketch = new p5(sketch, "main");
 
-  let tempMatrix = "ff0000551a8bff0000ff0100551a8b551a8bff0000ff0100ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ffff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ffff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ffff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff";
-  let tempSize = [9,13];
-  p5sketch.updateMatrix('something',tempSize, tempMatrix, false);
-}
+  let tempMatrix =
+    "ff0000551a8bff0000ff0100551a8b551a8bff0000ff0100ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ffff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ffff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ffff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff000000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff0000ff";
+  let tempSize = [9, 13];
+  p5sketch.updateMatrix("something", tempSize, tempMatrix, false);
+};
 
-loadData = function(){
-
-  p5sketch = new p5(sketch, 'main');
+loadData = function() {
+  p5sketch = new p5(sketch, "main");
 
   var user = firebase.auth().currentUser;
 
-  return firebase.database().ref('users/' + user.uid).once('value')
-  .then(function(snapshot) {
-    if (snapshot.val() == null) { // create new user
+  return firebase
+    .database()
+    .ref("users/" + user.uid)
+    .once("value")
+    .then(function(snapshot) {
+      if (snapshot.val() == null) {
+        // create new user
 
+        //populate matrix array (just once?)
+        let temp = [];
+        for (let i = 0; i < 120; i++) temp[i] = [0, 0, 0, 0];
+        console.log(JSON.stringify(temp));
+      } else {
+        return snapshot.val().matrixref;
+      }
+    })
+    .then(function(matrixref) {
+      console.log(matrixref);
+      if (matrixref != "") {
+        // Attach a listener to the Matrix state. This way, we update the database and the webpage and LED matrix should update at similar speeds
+        // This is alternative to update the visual on screen and then update the database.
 
-      //populate matrix array (just once?)
-      let temp = [];
-      for (let i = 0; i < 120; i++) temp[i] = [0,0,0,0];
-      console.log(JSON.stringify(temp));
+        return firebase
+          .database()
+          .ref("matrices/" + matrixref)
+          .on("value", function(snapshot) {
+            let content = snapshot.val();
 
-    } else {
-      return snapshot.val().matrixref;
-    }
-  })
-  .then(function(matrixref){
-    console.log(matrixref)
-    if (matrixref != ""){
+            p5sketch.updateMatrix(
+              matrixref,
+              [content.size.width, content.size.height],
+              content.LED
+            );
 
-      // Attach a listener to the Matrix state. This way, we update the database and the webpage and LED matrix should update at similar speeds
-      // This is alternative to update the visual on screen and then update the database.
-
-
-
-      return firebase.database().ref('matrices/' + matrixref).on('value', function(snapshot){
-        let content = snapshot.val();
-
-        p5sketch.updateMatrix(matrixref, [content.size.width, content.size.height], content.LED);
-
-        let matrixsize = content.size;
-      });
-
-    }
-  });
+            let matrixsize = content.size;
+          });
+      }
+    });
 };
 
-updateMatrixDatabase = function (matrixref, newMatrixString) {
-  return firebase.database().ref('matrices/' + matrixref).update({
-    'LED': newMatrixString
-  }, function(error) {
-    if (error) {
-      alert("Data could not be saved." + error);
-    } else {
-      alert("Data saved successfully.");
-    }
-  });
-}
+updateMatrixDatabase = function(matrixref, newMatrixString) {
+  return firebase
+    .database()
+    .ref("matrices/" + matrixref)
+    .update(
+      {
+        LED: newMatrixString
+      },
+      function(error) {
+        if (error) {
+          alert("Data could not be saved." + error);
+        } else {
+          alert("Data saved successfully.");
+        }
+      }
+    );
+};
+
+isMobileDevice = function() {
+  return (
+    typeof window.orientation !== "undefined" ||
+    navigator.userAgent.indexOf("IEMobile") !== -1
+  );
+};
 
 /*  HELPER FUNCTIONS  */
 
 // convert int (0 - 254) to 2 letter hex (always 2 letters!)
 // based on https://campushippo.com/lessons/how-to-convert-rgb-colors-to-hexadecimal-with-javascript-78219fdb
-let rgbToHex = function (colorvalue) {
+let rgbToHex = function(colorvalue) {
   var hex = Number(colorvalue).toString(16);
   if (hex.length < 2) {
     hex = "0" + hex;
@@ -106,9 +122,9 @@ let rgbToHex = function (colorvalue) {
 
 // convert three int values into a 6 letter HEX string
 // based on https://campushippo.com/lessons/how-to-convert-rgb-colors-to-hexadecimal-with-javascript-78219fdb
-let fullColorHex = function(r,g,b) {
+let fullColorHex = function(r, g, b) {
   var red = rgbToHex(r);
   var green = rgbToHex(g);
   var blue = rgbToHex(b);
-  return red+green+blue;
+  return red + green + blue;
 };
--- public/js/sketch_withselect.js
+++ public/js/sketch_withselect.js
@@ -9,247 +9,283 @@ TODOs:
 
 */
 
-let sketch = function(p){
+let sketch = function(p) {
+  let paths = [],
+    LEDS = [],
+    interactivity = []; // All the paths (or zones), leds and connected interactivities
 
-  let paths = [], LEDS = [], interactivity = [];  // All the paths (or zones), leds and connected interactivities
+  let touch = false; // on setup we check if we work with a mobile device.
+  let selecting = 0; // Are we selecting LEDs? (use millis to indicate unique identifyer)
+  let previous_selecting = 0; // to see if we have a new click
+  let selectingmode = 0; // 0 is not yet defined (depends on first LED that is selected), 1 is selecting, -1 is deselecting (as kind of eraser mode)
+  let selectingPath = [[]]; // selectable zones will be LEDmatrixLength long (adjusting values in an array is more efficient than adding / splicing)
 
-  let selecting = 0;;           // Are we selecting LEDs? (use millis to indicate unique identifyer)
-  let previous_selecting = 0;   // to see if we have a new click
-  let selectingmode = 0;        // 0 is not yet defined (depends on first LED that is selected), 1 is selecting, -1 is deselecting (as kind of eraser mode)
-  let selectingPath = -1;       // which 'zone' / path we are working with
+  let lastDrawn = 0; // keep track of when drawn
+  let drawnRate = 100; // 'framerate' for drawing LEDs
 
-  let lastDrawn = 0;            // keep track of when drawn
-  let drawnRate = 100;          // 'framerate' for drawing LEDs
+  let selectmargin = 10; // in pixels. Avoids loads of checks at the same spot. Is calculated as LEDspacing / x (see updateMatrix);
 
-  let selectmargin = 5;         // in pixels. Avoids loads of checks at the same spot
+  let current_point; // Where are we now and where were we?
+  let previous_point; // where were we?
+  let missed_points = []; // extrapolate points if we move faster than the frame rate
 
-  let current_point;             // Where are we now and where were we?
-  let previous_point;            // where were we?
-  let missed_points = [];        // extrapolate points if we move faster than the frame rate
-
-  let WINDOWsize = [];             // size of the canvas
+  let WINDOWsize = []; // size of the canvas
   let LEDspacing;
-  let LEDmatrixSize = [0,0];  // size of the LED matrix we are configuring
-  let LEDmatrixString = "";  // keep a latest version of the STRING from the database, so we can re-render the matrix when needed.
+  let LEDmatrixSize = [0, 0]; // size of the LED matrix we are configuring
+  let LEDmatrixString = ""; // keep a latest version of the STRING from the database, so we can re-render the matrix when needed.
   let LEDmatrixLength = 0;
-  let LEDsize = .8;
+  let LEDsize = 0.8;
   let LEDmatrixName = "unknown";
 
-  let resizeMatrix;       // variable to set timeout to recalc matrix (with delay)
+  let resizeMatrix; // variable to set timeout to recalc matrix (with delay)
 
-  const triggers = document.getElementById('triggers');
+  const triggers = document.getElementById("triggers");
+  const drawmode = document.getElementById("drawmode");
 
   p.setup = function() {
     WINDOWsize = p.getCanvasSize();
     let canvas = p.createCanvas(WINDOWsize[0], WINDOWsize[1]);
-    canvas.id('p5Canvas');
-    //canvas.mouseMoved(function(){p.checkLEDhit();}); // attach listener for // activity on canvas only
+    canvas.id("p5Canvas");
 
     current_point = p.createVector(0, 0);
 
     // testing with one zone and first zone selected
-    paths.push([]);   // first path to start with for now
+    paths.push([]); // first path to start with for now
     selectingPath = 0;
-  }
 
+    touch = isMobileDevice(); // function in the iotcanvas.js
+    console.log("touchmode: " + touch);
+  };
 
-  p.getCanvasSize = function (){
-    let div = document.getElementById('main');
-    return [div.offsetWidth,div.offsetHeight];
-  }
+  p.getCanvasSize = function() {
+    let div = document.getElementById("main");
+    return [div.offsetWidth, div.offsetHeight];
+  };
 
   p.draw = function() {
-
-    if (p.millis()-lastDrawn > drawnRate) {
+    if (p.millis() - lastDrawn > drawnRate) {
       lastDrawn = p.millis();
       p.drawView();
     }
 
-    current_point.set(p.mouseX, p.mouseY);
-
-    if (selecting > 0){   // only check if we substantially changed position or selecting has been false ( we have released)
+    if (selecting > 0) {
+      current_point.set(p.mouseX, p.mouseY);
       let distance = current_point.dist(previous_point);
-      if (current_point.dist(previous_point) > selectmargin) {
-        let steps = Math.ceil(distance/selectmargin);
+      if (distance > selectmargin) {
+        // only check if we substantially changed position or selecting has been false ( we have released)
 
-        for(let i = 0; i < steps; i++) missed_points.push(p5.Vector.lerp(previous_point, current_point, (i/steps)));     // extrapolate between current and 
-        for (let i = 0; i < missed_points.length; i++) p.selectLEDS(missed_points[i]);
+        let steps = Math.ceil(distance / selectmargin);
+        for (let i = 0; i < steps; i++) {
+          p.checkLEDhit(
+            p5.Vector.lerp(previous_point, current_point, i / steps)
+          ); // extrapolate between current and previous points to ensure we get all possible led's hit
+        }
 
-
-        missed_points = [];
         previous_point = current_point.copy();
       }
     }
-  }
+  };
 
   /* Checking the input on every few pixel moves was jittery when drawing every frame. Now, we only draw the changes */
 
-  p.drawView = function(){
+  p.drawView = function() {
     p.background(200);
     p.fill(0);
     p.noStroke();
 
-    for (let i = 0; i < LEDS.length; i++) LEDS[i].display();    // Draw all LEDs
-  }
+    for (let i = 0; i < LEDS.length; i++) LEDS[i].display(); // Draw all LEDs
+  };
 
-  p.selectLEDS = function(position){
-    for (let i = 0; i < LEDS.length; i++) {
-      if (LEDS[i].select(position)){        // returns boolean if found
-
-        let ledIsAt = paths[selectingPath].indexOf(i); // returns -1 if not found
-
-        if (ledIsAt > -1 && selectingmode <= 0) {   // we already have it, so we will delete it
-          selectingmode = -1;
-          paths[selectingPath].splice(ledIsAt, 1);
-          LEDS[i].highlighting(false);
-        } else if (ledIsAt == -1 && selectingmode >= 0){              // we don't have it, so add it and go in adding mode
-          selectingmode = 1;
-          paths[selectingPath].push(i);
-          LEDS[i].highlighting(true);
-        }
-        //return true;  // break the loop if found
-      }
-    }
+  p.mousePressed = function() {
+    if (!touch) p.screenPressed();
     return false;
-  }
-
-  p.mousePressed = function(){
+  };
+  p.touchStarted = function() {
     p.screenPressed();
-  }
-  p.touchStarted = function(){
-    p.screenPressed();
-    console.log("touch!!");
-  }
+    //console.log("touch!!");
+    return false;
+  };
 
-  p.mouseReleased = function(){
+  p.mouseReleased = function() {
+    if (!touch) p.screenReleased();
+    return false;
+  };
+
+  p.touchEnded = function() {
     p.screenReleased();
-  }
+    //console.log("touch!!");
+    return false;
+  };
 
-  p.touchEnded = function(){
-    p.screenReleased();
-    console.log("touch!!");
-  }
-
-  p.relativeToMatrix = function(position){
-    let rel_x = (position.x - WINDOWsize[0]/2 - LEDspacing/2) / LEDspacing;
-    let rel_y = (position.y - WINDOWsize[1]/2 - LEDspacing/2) / LEDspacing;
+  p.relativeToMatrix = function(position) {
+    let rel_x = (position.x - WINDOWsize[0] / 2 - LEDspacing / 2) / LEDspacing;
+    let rel_y = (position.y - WINDOWsize[1] / 2 - LEDspacing / 2) / LEDspacing;
     return p.createVector(rel_x, rel_y);
-  }
+  };
 
-  p.relativeToView = function(position){   // margin is overridden for LEDs
-    let rel_x = position.x * LEDspacing + WINDOWsize[0]/2 + LEDspacing/2;
-    let rel_y = position.y * LEDspacing + WINDOWsize[1]/2 + LEDspacing/2;
+  p.relativeToView = function(position) {
+    // margin is overridden for LEDs
+    let rel_x = position.x * LEDspacing + WINDOWsize[0] / 2 + LEDspacing / 2;
+    let rel_y = position.y * LEDspacing + WINDOWsize[1] / 2 + LEDspacing / 2;
     return p.createVector(rel_x, rel_y);
-  }
+  };
 
-  p.updateMatrix = function(matrixref = LEDmatrixName, new_size = LEDmatrixSize, new_string = LEDmatrixString, new_window = false){    // default values if undefined: [], "", boolean
+  p.updateMatrix = function(
+    matrixref = LEDmatrixName,
+    new_size = LEDmatrixSize,
+    new_string = LEDmatrixString,
+    new_window = false
+  ) {
+    // default values if undefined: [], "", boolean
     let new_array = new_string.match(/.{1,6}/g);
     // if we get new metrics (e.g. the first time)
-    if (new_size[0] != LEDmatrixSize[0] || new_size[1] != LEDmatrixSize[1] /* matrix dimensions changed */
-      || new_array.length != LEDmatrixLength             /* number of LEDs changed, though new_size should then also flag*/
-      || new_window  /* window is resized, easier to recalc instead of adjust positions */
-    ){
+    if (
+      new_size[0] != LEDmatrixSize[0] ||
+      new_size[1] != LEDmatrixSize[1] /* matrix dimensions changed */ ||
+      new_array.length !=
+        LEDmatrixLength /* number of LEDs changed, though new_size should then also flag*/ ||
+      new_window /* window is resized, easier to recalc instead of adjust positions */
+    ) {
       LEDmatrixName = matrixref;
       LEDmatrixSize = new_size;
       LEDmatrixString = new_string;
       LEDmatrixLength = LEDmatrixSize[0] * LEDmatrixSize[1];
       WINDOWsize = p.getCanvasSize();
-      LEDspacing = p.min(WINDOWsize[0]/(LEDmatrixSize[0]+2),WINDOWsize[1]/(LEDmatrixSize[1]+2));
+      LEDspacing = p.min(
+        WINDOWsize[0] / (LEDmatrixSize[0] + 2),
+        WINDOWsize[1] / (LEDmatrixSize[1] + 2)
+      );
+
+      // fill our temporary selecting Array! TODO make this dynamic!
+      paths[0] = new Array(LEDmatrixLength);
+      for (let i = 0; i < LEDmatrixLength; i++) paths[0][i] = 0;
+
+      selectmargin = Math.ceil(LEDspacing / 4); // through trial and error, this seems to get the most promising results (almost never misses a LED if touched)
 
       p.resizeCanvas(WINDOWsize[0], WINDOWsize[1]);
 
-      let topleft_x = WINDOWsize[0]/2 - (LEDmatrixSize[0]*LEDspacing)/2 + LEDspacing/2;
-      let topleft_y = WINDOWsize[1]/2 - (LEDmatrixSize[1]*LEDspacing)/2 + LEDspacing/2;
+      let topleft_x =
+        WINDOWsize[0] / 2 -
+        (LEDmatrixSize[0] * LEDspacing) / 2 +
+        LEDspacing / 2;
+      let topleft_y =
+        WINDOWsize[1] / 2 -
+        (LEDmatrixSize[1] * LEDspacing) / 2 +
+        LEDspacing / 2;
 
-      LEDS = [];  // empty array, then create a new one based on this data
+      LEDS = []; // empty array, then create a new one based on this data
       let iterator = 0;
-      for (let x = 0; x < LEDmatrixSize[0]; x++){
-        for (let y = 0; y < LEDmatrixSize[1]; y++){
-          LEDS.push(new LED(x - (LEDmatrixSize[0]/2), y - (LEDmatrixSize[1]/2), new_array[iterator], LEDsize, LEDspacing, p));
+      for (let x = 0; x < LEDmatrixSize[0]; x++) {
+        for (let y = 0; y < LEDmatrixSize[1]; y++) {
+          LEDS.push(
+            new LED(
+              x - LEDmatrixSize[0] / 2,
+              y - LEDmatrixSize[1] / 2,
+              new_array[iterator],
+              LEDsize,
+              LEDspacing,
+              p
+            )
+          );
           iterator++;
         }
         x++;
-        if (x < LEDmatrixSize[0]){
-          for (let y = LEDmatrixSize[1]-1; y >= 0; y--){
-            LEDS.push(new LED(x - (LEDmatrixSize[0]/2), y - (LEDmatrixSize[1]/2), new_array[iterator], LEDsize, LEDspacing, p));
+        if (x < LEDmatrixSize[0]) {
+          for (let y = LEDmatrixSize[1] - 1; y >= 0; y--) {
+            LEDS.push(
+              new LED(
+                x - LEDmatrixSize[0] / 2,
+                y - LEDmatrixSize[1] / 2,
+                new_array[iterator],
+                LEDsize,
+                LEDspacing,
+                p
+              )
+            );
             iterator++;
           }
         }
       }
     } else {
       console.log(new_string);
-      if (new_string.localeCompare(LEDmatrixString) != 0){      // only the colours changed! Update colour of LEDs
+      if (new_string.localeCompare(LEDmatrixString) != 0) {
+        // only the colours changed! Update colour of LEDs
         LEDmatrixString = new_string;
-        for (let i = 0; i < LEDmatrixLength-1; i++){
+        for (let i = 0; i < LEDmatrixLength - 1; i++) {
           LEDS[i].updateColor(new_array[i]);
         }
       }
     }
-  }
+  };
 
-  p.windowResized = function(){
+  p.windowResized = function() {
     // wait 1 second for any windowsize changes (by overriding any ongoing windowchanges)
     window.clearTimeout(resizeMatrix);
-    resizeMatrix = window.setTimeout(function(){
+    resizeMatrix = window.setTimeout(function() {
       p.updateMatrix(LEDmatrixName, LEDmatrixSize, LEDmatrixString, true);
     }, 500);
-  }
+  };
 
   // create a new element for in the DOM - I am not checking for duplicates at the moment
-  p.createTrigger = function(name, id){
-
+  p.createTrigger = function(name, id) {
     let toggle = document.createElement("li");
     let input = document.createElement("INPUT");
-    input.setAttribute("type","checkbox");
+    input.setAttribute("type", "checkbox");
     input.name = name;
     input.value = id;
-    let label = document.createElement("LABEL")
+    let label = document.createElement("LABEL");
     label.htmlFor = name;
     let innerLabel = document.createTextNode(name);
     label.appendChild(innerLabel);
     toggle.appendChild(input);
     toggle.appendChild(label);
 
-    let list  = document.getElementById("triggers");
+    let list = document.getElementById("triggers");
     list.appendChild(toggle);
 
-    input.addEventListener('change', () => {
-      for (let i = 0; i < interactivity.length; i++){
-        if (interactivity[i].id == input.value){
+    input.addEventListener("change", () => {
+      for (let i = 0; i < interactivity.length; i++) {
+        if (interactivity[i].id == input.value) {
           interactivity[i].active = input.checked;
 
-          for (let q = 0; q < LEDS.length; q++){
+          for (let q = 0; q < LEDS.length; q++) {
             LEDS[i].updateTrigger(interactivity[i].id, interactivity[i].active);
           }
-
         }
       }
-
-    })
-  }
-
+    });
+  };
 
   p.screenPressed = function() {
     selecting = p.millis();
 
     current_point.x = p.mouseX;
-    current_point.y =  p.mouseY;   // Grab mouse position
+    current_point.y = p.mouseY; // Grab mouse position
     previous_point = current_point.copy();
 
-    p.selectLEDS(current_point);
-  }
+    p.checkLEDhit(current_point);
+  };
 
-  p.checkLEDhit = function(){
-    current_point.set(p.mouseX, p.mouseY);
+  p.checkLEDhit = function(clicked) {
+    let point = p.relativeToMatrix(clicked);
+    let x = Math.round(point.x + LEDmatrixSize[0] / 2);
+    let y = Math.round(point.y + LEDmatrixSize[1] / 2);
+    if (x >= 0 && x < LEDmatrixSize[0] && y >= 0 && y < LEDmatrixSize[1]) {
+      // we are within the grid! this should result in one possible anwer due to our rounding
+      let selectedLED;
+      if (x % 2) selectedLED = x * LEDmatrixSize[1] + LEDmatrixSize[1] - y - 1;
+      // = 1, so we are in even row (0, 1, 2, 3, 4, etc..)
+      else selectedLED = x * LEDmatrixSize[1] + y; // = 0, so we are in an uneven row.
 
-    if (selecting > 0){   // only check if we substantially changed position or selecting has been false ( we have released)
-      let distance = current_point.dist(previous_point);
-      if (current_point.dist(previous_point) > selectmargin) {
-        p.selectLEDS(current_point);
-        previous_point = current_point.copy();
+      if (drawmode.checked) {
+        paths[selectingPath][selectedLED] = 1;
+        LEDS[selectedLED].highlighting(true);
+      } else {
+        paths[selectingPath][selectedLED] = 0;
+        LEDS[selectedLED].highlighting(false);
       }
     }
-  }
+  };
 
   p.screenReleased = function() {
     if (selecting) {
@@ -296,20 +332,19 @@ let sketch = function(p){
       selectingmode = 0;
 
       p.updateFirebase();
-
     }
-  }
+  };
 
-  p.updateLEDstring = function(){
+  p.updateLEDstring = function() {
     let stringArray = [];
     for (let i = 0; i < LEDS.length; i++) {
       stringArray.push(LEDS[i].toString());
     }
     //console.log(stringArray.join(""));
-  }
+  };
 
-  p.updateFirebase = function(){
+  p.updateFirebase = function() {
     p.updateLEDstring();
     //updateMatrixDatabase(LEDmatrixName, LEDmatrixString);
-  }
-}
+  };
+};
